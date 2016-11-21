941205dffb94701613d6928480abd9fa38971db5
Daniel Zimmermann <daniel.zimmermann@serraview.com>
2016-11-21 18:57:39 +1100

    Clean up test a touch

--- GraphQL.DataLoader.Tests/DataLoaderContextTests.cs
+++ GraphQL.DataLoader.Tests/DataLoaderContextTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Shouldly;
 using Xunit;
@@ -35,18 +36,17 @@ namespace GraphQL.DataLoader.Tests
         [Fact]
         public void DataLoaderContext_Flush_CanHandleMultipleLevelsOfNestedFetches()
         {
-            string result1;
-            string result2;
-            string result3;
-
-            var result = DataLoaderContext.Run(async () =>
+            Should.NotThrow(() =>
             {
-                var loader = new DataLoader<int, string>(ints => ints.ToDictionary(x => x, x => $"SomeResult{x}"));
-                result1 = await loader.LoadAsync(1);
-                result2 = await loader.LoadAsync(2);
-                result3 = await loader.LoadAsync(3);
-                return new[] {result1, result2, result3};
-            }).Result;
+                DataLoaderContext.Run(async () =>
+                {
+                    var loader = new DataLoader<int, string>(ints => ints.ToDictionary(x => x, x => $"SomeResult{x}"));
+                    await loader.LoadAsync(1);
+                    await loader.LoadAsync(2);
+                    await loader.LoadAsync(3);
+                    await loader.LoadAsync(4);
+                });
+            });
         }
 
         [Fact]
